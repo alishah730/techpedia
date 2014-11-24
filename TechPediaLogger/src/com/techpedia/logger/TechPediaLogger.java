263ee7f1c6ad9ab82afd69e6c35b59d00e90e4b5
685679 <Manish6.Kumar@ge.com>
2014-11-24 19:15:14 +0530

    changed the serialization serial number

--- TechPediaLogger/src/com/techpedia/logger/TechPediaLogger.java
+++ TechPediaLogger/src/com/techpedia/logger/TechPediaLogger.java
@@ -3,10 +3,11 @@ import org.apache.logging.log4j.core.Logger;
 
 
 public class TechPediaLogger extends Logger{
+
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -3322235225498326070L;
 
 	private TechPediaLogger(String fqcn){
 		super(TechPediaLoggerContext.getContext(),fqcn, TechPediaLoggerContext.getMessageFactory(fqcn) );
